8cc297622bde6227cdd1f092b320e0b0005f66a1
feyzaozden <fyzozden@gmail.com>
2025-01-07 15:55:47 +0300

    some minor fixes

--- CampusLifeRPG/Gym.cs
+++ CampusLifeRPG/Gym.cs
@@ -10,9 +10,9 @@
         public override void Visit(Player player)
         {
             UpdatePlayerStats(player,
-                energy: -60,    // Enerji azalır
+                energy: -50,    // Enerji azalır
                 intelligence: 0,
-                social: 20,     // Sosyallik biraz artar
+                social: 25,     // Sosyallik biraz artar
                 health: 40);    // Sağlık çok artar
 
             System.Console.WriteLine("Spor salonunda iyi bir antrenman yaptınız. Sağlığınız arttı ama yoruldunuz.");
